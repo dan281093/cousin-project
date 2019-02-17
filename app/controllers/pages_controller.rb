6ae290b08cb7a95c1cea95c98ce86e3e5062b30b
Daniel Tridico <mail@danieltridico.com>
2019-02-17 19:13:13 +0200

    rewrote the routes, got rid of user controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-
   def home
+  end
+
+  def my_services
+    @user = current_user
+    @services = @user.services
+  end
+
+  def my_bookings
   end
 end
