e92771636d71ab2024c50f7d875cce0bffd085d1
dan amouyal <danamouyal@hotmail.fr>
2019-02-17 20:16:46 +0200

    Add form for services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,6 +4,25 @@ class ServicesController < ApplicationController
   end
 
   def show
-    @services = service.find(params[:id])
+    @services = Service.find(params[:id])
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @services = Service.new(services_params)
+    if @services.save
+      redirect_to my_services_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def services_params
+    params.require(:service).permit(:name, :description)
   end
 end
