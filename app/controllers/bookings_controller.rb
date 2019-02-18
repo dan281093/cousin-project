9205cbdd8a93ad10c0ed577285f3c1ddca4780b0
Daniel Tridico <mail@danieltridico.com>
2019-02-18 13:48:11 +0200

    created new bookings page. Creating a new booking works here

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,35 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update]
+  before_action :set_service, only: [:new, :create]
   def index
     @bookings = Booking.all
   end
 
   def new
-    @bookings = Booking.new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.service = @service
+    @booking.user = current_user
+    @booking.save!
+    redirect_to my_bookings_path
+
+    # Also set price of the booking!
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_service
+    @service = Service.find(params[:service_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:description, :time)
   end
 end
