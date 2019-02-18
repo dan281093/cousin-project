9d8a949f6b7a29859e5d7bb3b2469249e8a3e74c
Daniel Tridico <45737337+dtricc@users.noreply.github.com>
2019-02-18 12:47:51 +0200

    Update service.rb
    spelling errors bro

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  CATEGORIES = ["Goverment Departments", "Forms", "Trips", "Health and Diet", "Neighborhoods"]
+  CATEGORIES = ["Government Departments", "Forms", "Trips", "Health and Diet", "Neighborhoods"]
   validates_inclusion_of :category, in: CATEGORIES
 end
