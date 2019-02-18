1afb7b3a387ea995bbd40ad7c179a6a8fa72927f
nickoma ceaser <nickomaceaser@gmail.com>
2019-02-18 10:51:13 +0200

    started making some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# users = User.create([{ name: 'Adam', email: 'Adam@test.com', password: 123456, address: 'Tel Aviv }, { name: 'Eve', email: 'eve@test.com', password: 123456, address: 'Tel Aviv }])
+#{ name: 'Don', email: 'Don@test.com', password: 123456, address: 'Tel Aviv }]),
+
+#services = Service.create([{ category: 'Goverment Departments', description: 'navigating govermentail services and departments', user_id: 1 }, { category: 'Forms', descriptions: 'Help filling out forms correctly in Hebrew and Arabic form English', user_id: 2 },
+#{ category: 'Trips', description: 'showing you how to go on day trip in Israel. When and where to go to certain places and what to avoid', user_id: 3 }, { category: 'Health and Diet' }, { category: 'Neighborhoods' }])
