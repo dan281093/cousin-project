5da06d094055c88fccf46e5624c7136a20c1ee05
nickoma ceaser <nickomaceaser@gmail.com>
2019-02-18 11:20:27 +0200

    seeds for user and services

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,61 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# users = User.create([{ name: 'Adam', email: 'Adam@test.com', password: 123456, address: 'Tel Aviv }, { name: 'Eve', email: 'eve@test.com', password: 123456, address: 'Tel Aviv }])
-#{ name: 'Don', email: 'Don@test.com', password: 123456, address: 'Tel Aviv }]),
+users = [
+  { name: 'Adam Black',
+    email: 'Adam@test.com',
+    password: 123456,
+    address: 'Tel Aviv'
+  },
 
-#services = Service.create([{ category: 'Goverment Departments', description: 'navigating govermentail services and departments', user_id: 1 }, { category: 'Forms', descriptions: 'Help filling out forms correctly in Hebrew and Arabic form English', user_id: 2 },
-#{ category: 'Trips', description: 'showing you how to go on day trip in Israel. When and where to go to certain places and what to avoid', user_id: 3 }, { category: 'Health and Diet' }, { category: 'Neighborhoods' }])
+  {
+    name: 'Eve'
+    , email: 'eve@test.com'
+    , password: 123456,
+    address: 'Tel Aviv' }
+
+{
+  name: 'Don',
+  email: 'Don@test.com',
+  password: 123456,
+  address: 'Tel Aviv' }],
+
+  User.create!(users)
+adam = User.find_by(name: "Adam Black")
+don = User.find_by(name: "Donald Trump")
+eve = User.find_by(name: "Eve Mendona")
+
+services =  [
+  {
+    category: 'Goverment Departments',
+  description: 'navigating govermentail services and departments',
+   user_id: 1
+ },
+
+ { category: 'Forms',
+  descriptions: 'Help filling out forms correctly in Hebrew and Arabic form English',
+   user_id: 1
+ },
+
+{
+  category: 'Trips',
+  description: 'showing you how to go on day trip in Israel. When and where to go to certain places and what to avoid',
+  user_id: 3 },
+
+{ category: 'Health and Diet',
+  description: 'Helping you find where to get your dietary needs, and where to find medicine and if you need a prescrition.'
+  user_id: 3 },
+{ category: 'Neighborhoods',
+  description: ' Will show you around the Neighborhood and let you know where who sells what and where the good buys are'
+  user_id: 1 }]
+
+  Service.create!(services)
+  gd = Service.find_by(category: 'Goverment Departments')
+  forms = Service.find_by(category: 'Forms')
+  trips Service.find_by(category: 'Trips')
+  hd Service.find_by(category: 'Health and Diet')
+  Neighborhood Service.find_by(category: 'HNeighborhoods')
+
+#
+
+
