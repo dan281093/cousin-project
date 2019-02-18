be9675d65f1bf73441c51c5442f105db5bebf265
nickoma ceaser <nickomaceaser@gmail.com>
2019-02-18 12:02:56 +0200

    seeds working and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+## Clear all services
+Service.destroy_all
+
+## clear old users
+User.destroy_all
+
 users = [
   { name: 'Adam Black',
     email: 'Adam@test.com',
-    password: 123456,
+    password: '123456',
     address: 'Tel Aviv'
   },
 
   {
-    name: 'Eve Mendona'
-    , email: 'eve@test.com'
-    , password: 123456,
-    address: 'Tel Aviv' }
+    name: 'Eve Mendona',
+    email: 'eve@test.com',
+    password: '123456',
+    address: 'Tel Aviv' },
 
 {
   name: 'Donald Trump',
   email: 'Don@test.com',
-  password: 123456,
-  address: 'Tel Aviv' }],
+  password: '123456',
+  address: 'Tel Aviv' }]
 
-  User.create!(users)
+ users.each do |user|
+ User.create!(user)
+end
 adam = User.find_by(name: "Adam Black")
 don = User.find_by(name: "Donald Trump")
 eve = User.find_by(name: "Eve Mendona")
@@ -34,32 +42,38 @@ services =  [
   {
     category: 'Goverment Departments',
   description: 'navigating govermentail services and departments',
-   user_id: 1
+   user_id: adam.id
  },
 
  { category: 'Forms',
-  descriptions: 'Help filling out forms correctly in Hebrew and Arabic form English',
-   user_id: 1
+  description: 'Help filling out forms correctly in Hebrew and Arabic form English',
+   user_id: adam.id
  },
 
 {
   category: 'Trips',
   description: 'showing you how to go on day trip in Israel. When and where to go to certain places and what to avoid',
-  user_id: 3 },
+  user_id: don.id
+},
 
 { category: 'Health and Diet',
-  description: 'Helping you find where to get your dietary needs, and where to find medicine and if you need a prescrition.'
-  user_id: 3 },
+  description: 'Helping you find where to get your dietary needs, and where to find medicine and if you need a prescrition.',
+  user_id: don.id
+},
 { category: 'Neighborhoods',
-  description: ' Will show you around the Neighborhood and let you know where who sells what and where the good buys are'
-  user_id: 1 }]
+  description: ' Will show you around the Neighborhood and let you know where who sells what and where the good buys are',
+  user_id: adam.id
+}]
 
-  Service.create!(services)
-  gd = Service.find_by(category: 'Goverment Departments')
-  forms = Service.find_by(category: 'Forms')
-  trips Service.find_by(category: 'Trips')
-  hd Service.find_by(category: 'Health and Diet')
-  Neighborhood Service.find_by(category: 'HNeighborhoods')
+services.each do |service|
+  Service.create!(service)
+end
+
+gd = Service.find_by(category: 'Goverment Departments')
+forms = Service.find_by(category: 'Forms')
+trips = Service.find_by(category: 'Trips')
+hd = Service.find_by(category: 'Health and Diet')
+neighborhood = Service.find_by(category: 'Neighborhoods')
 
 #
 
