c6ef80190aaf4c72e8ad9e9e1f322c40f2012e09
Daniel Tridico <mail@danieltridico.com>
2019-02-18 14:36:58 +0200

    added my_service page as well

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,12 @@ class PagesController < ApplicationController
   end
 
   def my_services
+    # maybe delete this part - who is using this?
     @user = current_user
     @services = @user.services
+    # delete the above
+
+    @my_services = Service.where(user_id: current_user.id)
   end
 
   def my_bookings
