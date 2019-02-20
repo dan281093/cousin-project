588442817e9d0b12d93889577bcd0d45ddbc74c6
nickoma ceaser <nickomaceaser@gmail.com>
2019-02-20 14:04:01 +0200

    show booking in progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,6 +9,10 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def create
     @booking = Booking.new(booking_params)
     @booking.service = @service
